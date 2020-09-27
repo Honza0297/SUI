f636208e33a0bb0399865b09e3cab0545de8925c
Jan Beran <jaberan@redhat.com>
2020-09-27 15:25:09 +0200

    Rename AI from xberan43 to DreamAI and make it a package (or something like that :))

--- dicewars/ai/DreamAI/DreamAI.py
+++ dicewars/ai/DreamAI/DreamAI.py
@@ -2,7 +2,7 @@ import random
 import logging
 import sys
 
-from dicewars.ai.utils import possible_attacks
+from dicewars.ai.utils import possible_attacks, probability_of_holding_area as can_hold, probability_of_successful_attack as should_attack, attack_succcess_probability
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 
@@ -21,11 +21,11 @@ class AI:
         If there are no more moves, the agent ends its turn.
         """
         attacks = list(possible_attacks(board, self.player_name))
-        if attacks:
-            self.logger.debug("Possible attacks are:")
-            self.logger.debug("   *"+attack for attack in attacks)
-            source, target = random.choice(attacks)
-            return BattleCommand(source.get_name(), target.get_name())
+        while attacks:
+            source, target = attacks.pop()
+            #if source.get_dice() > target.get_dice():
+            if attack_succcess_probability(source.get_dice(), target.get_dice()) > 0.5: 
+                return BattleCommand(source.get_name(), target.get_name())
         else:
             self.logger.debug("No more possible turns.")
             return EndTurnCommand()
--- scripts/dicewars-tournament.py
+++ scripts/dicewars-tournament.py
@@ -47,7 +47,7 @@ PLAYING_AIs = [
     # 'dt.wpm_d',
     # 'dt.wpm_s',
     'dt.wpm_c',
-    # 'xlogin42',
+    'DreamAI',
     'xlogin00',
 ]
 UNIVERSAL_SEED = 42
