ea13fff28c72b8fa0aa3473065526d32bf49fc44
Daniel Bubeníček <xbuben05@stud.fit.vutbr.cz>
2020-12-19 19:23:31 +0100

    WIP

--- dicewars/ai/DreamAI/DreamAI.py
+++ dicewars/ai/DreamAI/DreamAI.py
@@ -5,7 +5,8 @@ import sys
 from dicewars.ai.utils import possible_attacks, probability_of_holding_area as can_hold, probability_of_successful_attack as should_attack, attack_succcess_probability
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
-
+from dicewars.client.game.board import Board
+from dicewars.client.game.player import Player
 
 class AI:
     def __init__(self, player_name, board, players_order):
@@ -14,12 +15,13 @@ class AI:
         # Pred odevzdanim zmenit na stderr!!!
         out_hdlr = logging.StreamHandler(sys.stdout)
         self.logger.addHandler(out_hdlr)
-
     def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, time_left):
         """AI agent's turn
         Get a random area. If it has a possible move, the agent will do it.
         If there are no more moves, the agent ends its turn.
         """
+        print("nejdelsi moje hranice:" + str(len(Helper.get_borders_of_largest_region(board, self.player_name)[0])))
+
         attacks = list(possible_attacks(board, self.player_name))
         while attacks:
             source, target = attacks.pop()
@@ -29,4 +31,36 @@ class AI:
         else:
             self.logger.debug("No more possible turns.")
             return EndTurnCommand()
+
+
+
+class Helper:
+    @staticmethod
+    def get_player_largest_region(board: Board, player_name):
+        """Get size of the largest region, including the areas within
+        Attributes
+        ----------
+        largest_region : list of int
+            Names of areas in the largest region
+        Returns
+        -------
+            List
+            Number of areas in the largest region
+        """
+        largest_region = []
+
+        players_regions = board.get_players_regions(player_name)
+        max_region_size = max(len(region) for region in players_regions)
+        max_sized_regions = [region for region in players_regions if len(region) == max_region_size]
+
+        largest_region = max_sized_regions[0]
+        return largest_region
+
+
+    @staticmethod
+    def get_borders_of_largest_region(board: Board, player_name):
+        region = Helper.get_player_largest_region(board, player_name)
+        return [area for area in region if board.is_at_border(area)]
+
+
 
